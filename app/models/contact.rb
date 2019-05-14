ccb0d69d06fd3db4fb95d123b77324da12f6c031
tiemae <roquerre.t@gmail.com>
2019-05-14 10:07:17 -0700

    adds middle_name and bio to database

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  
+  def friendly_updated_at
+    updated_at.strftime("%A, %b %d")
+  end
+def full_name
+  "#{first_name} #{last_name}"
+end
+
+def japan_phone_number
+  "+81 #{phone_number}"
+end
 end
---
+++ db/migrate/20190514165444_add_middlename_to_contacts.rb
@@ -0,0 +1,5 @@
+class AddMiddlenameToContacts < ActiveRecord::Migration[5.2]
+  def change
+    add_column :contacts, :middle_name, :string
+  end
+end
